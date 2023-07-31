08ff0419deedcaba186856ed57b5336754d99b18
fatmachamtouri <chamtouriefatma@gmail.com>
2023-07-31 18:22:08 +0100

    first commit

--- src/axios/Api.js
+++ src/axios/Api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-axios.defaults.baseURL = 'http://localhost:4000/api/';
+axios.defaults.baseURL = 'https://backend-delta-silk.vercel.app/';
 axios.interceptors.request.use(
     config => {
         const token = localStorage.getItem("CC_Token")
